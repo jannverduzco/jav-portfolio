b054ae48573f39a57e82a0d3834d6472c92b2ffa
Janneth Verduzco <janneth_verduzco@live.com>
2021-04-18 22:46:51 -0500

    Added initial components (About, Contact, Projects)

---
+++ src/Componets/About.js
@@ -0,0 +1,13 @@
+import React from "react"
+ 
+function About(props) {
+  return (
+    <div>
+      <h1>Hi!
+        <p>My name is Janneth and I am a Full Stack Web</p>
+      </h1>
+    </div>
+  )
+}
+ 
+export default About;
---
+++ src/Componets/Projects.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function Projects(props) {
+  return (
+    <div>
+      <h1>Project List</h1>
+    </div>
+  )
+}
+ 
+export default Projects;
