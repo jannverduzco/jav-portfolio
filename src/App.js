1cc7817f12397d3a55d2796992fcf2b5b0785ea7
Janneth Verduzco <janneth_verduzco@live.com>
2021-04-19 14:22:12 -0500

    Imported main components into App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import './App.css';
+import "./App.css";
+import About from "./Componets/About";
+import Projects from "./Componets/Projects";
+import Contact from "./Componets/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <About />
+      <Projects />
+      <Contact />
     </div>
   );
 }
