3d01a0a0b32472490db6ffac8ff4de85427552cf
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-05 22:10:01 -0500

    Added my picture in the 'About Me' component along with styling

--- src/Componets/About/index.js
+++ src/Componets/About/index.js
@@ -1,16 +1,35 @@
 import React from "react";
+import "./style.css";
 import Image from "../../images/about-me.jpg";
+import JannethV from "../../images/jannethaverduzco.jpg";
 
 function About() {
   return (
-    <div className="section-container">
+    <div className="container">
       <div
         style={{
           backgroundImage: `url(${Image})`,
           backgroundSize: "cover",
         }}
       >
-        <h5> I am a Full Stack Web Developer.</h5>
+        <h1>About Me</h1>
+        <img src={JannethV} alt="Janneth Verduzco" height={200} width={200} />
+
+        <div className="row">
+          <div className="col-md-10">
+            <p>
+              I live in San Antonio, Texas, where I graduated from The
+              University of Texas with a Biology Degree. I used my degree to
+              focus on clinical trials. Four years post-graduation I have
+              received my certificate, also from UTSA, as a Full Stack Web
+              Developer.
+            </p>
+            <p>
+              I have experience as a front and back end with a stack in
+              JavaScript, React, MongoDB, MySQL, NoSQL, GitHub.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
