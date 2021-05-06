74fb8303ed314ff270b711320a9e1aa7534af280
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-05 22:44:54 -0500

    Added @media query to make 'About Me' component resposive

--- src/Componets/About/index.js
+++ src/Componets/About/index.js
@@ -1,34 +1,22 @@
 import React from "react";
 import "./style.css";
-import Image from "../../images/about-me.jpg";
 import JannethV from "../../images/jannethaverduzco.jpg";
 
 function About() {
   return (
     <div className="container">
-      <div
-        style={{
-          backgroundImage: `url(${Image})`,
-          backgroundSize: "cover",
-        }}
-      >
-        <h1>About Me</h1>
-        <img src={JannethV} alt="Janneth Verduzco" height={200} width={200} />
-
-        <div className="row">
-          <div className="col-md-10">
-            <p>
-              I live in San Antonio, Texas, where I graduated from The
-              University of Texas with a Biology Degree. I used my degree to
-              focus on clinical trials. Four years post-graduation I have
-              received my certificate, also from UTSA, as a Full Stack Web
-              Developer.
-            </p>
-            <p>
-              I have experience as a front and back end with a stack in
-              JavaScript, React, MongoDB, MySQL, NoSQL, GitHub.
-            </p>
-          </div>
+      <h1>About Me</h1>
+      <div className="row about-me-section">
+        <div className="col-sm-3 img-div">
+          <img src={JannethV} alt="Janneth Verduzco" height={200} width={200} />
+        </div>
+        <div className="col-sm-6">
+          I live in San Antonio, Texas, where I graduated from The University of
+          Texas with a Biology Degree. I used my degree to focus on clinical
+          trials. Four years post-graduation I have received my certificate,
+          also from UTSA, as a Full Stack Web Developer. I have experience as a
+          front and back end with a stack in JavaScript, React, MongoDB, MySQL,
+          NoSQL, GitHub.
         </div>
       </div>
     </div>
