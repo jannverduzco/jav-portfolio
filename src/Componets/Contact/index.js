369c172a6aa79a69e1d575716f7de15b58581ec2
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-16 15:44:35 -0500

    Added Contact Component form

--- src/Componets/Contact/index.js
+++ src/Componets/Contact/index.js
@@ -1,11 +1,41 @@
-import React from "react"
- 
+import React from "react";
+import { Container } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
 function Contact() {
   return (
-    <div>
-      <h5>Contact Form</h5>
-    </div>
-  )
+    <Container className="container">
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} md="6" controlId="validationCustom01">
+            <Form.Label>First name</Form.Label>
+            <Form.Control required type="text" placeholder="First" />
+          </Form.Group>
+          <Form.Group as={Col} md="6" controlId="validationCustom02">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control required type="text" placeholder="Last" />
+          </Form.Group>
+        </Form.Row>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Enter Message Here..."
+            rows={3}
+          />
+        </Form.Group>
+        <Button variant="warning" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
- 
+
 export default Contact;
