567116211d0a3081a6eb6083bed841ae1f8a5d1f
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-16 15:27:42 -0500

    Updated about me professional picture

--- src/Componets/About/index.js
+++ src/Componets/About/index.js
@@ -7,8 +7,8 @@ function About() {
     <div className="container">
       <h2>About Me</h2>
       <div className="row about-me-section">
-        <div className="col-md-4d">
-          <img src={JannethV} alt="Janneth Verduzco" height={320} width={320} />
+        <div className="col-md-5">
+        <img src={JannethV} alt="Janneth Verduzco" height={320} width={420} />
         </div>
         <div className="col-md-6">
           I live in San Antonio, Texas, where I graduated from The University of
