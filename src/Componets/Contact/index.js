9a176c6af2175ef21ddf7697ef00b62e6320d0c6
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-16 16:01:11 -0500

    Styled form section titles for more gradinet

--- src/Componets/Contact/index.js
+++ src/Componets/Contact/index.js
@@ -3,27 +3,29 @@ import { Container } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import "./style.css";
 
 function Contact() {
   return (
     <Container className="container">
+      <h2>Contact Me</h2>
       <Form>
         <Form.Row>
           <Form.Group as={Col} md="6" controlId="validationCustom01">
-            <Form.Label>First name</Form.Label>
+            <h6>First Name</h6>
             <Form.Control required type="text" placeholder="First" />
           </Form.Group>
           <Form.Group as={Col} md="6" controlId="validationCustom02">
-            <Form.Label>Last name</Form.Label>
+            <h6>Last Name</h6>
             <Form.Control required type="text" placeholder="Last" />
           </Form.Group>
         </Form.Row>
         <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
+          <h6>Email Address</h6>
           <Form.Control type="email" placeholder="name@example.com" />
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Message</Form.Label>
+          <h6>Message</h6>
           <Form.Control
             as="textarea"
             placeholder="Enter Message Here..."
@@ -31,7 +33,7 @@ function Contact() {
           />
         </Form.Group>
         <Button variant="warning" type="submit">
-          Submit
+          <h6>Submit</h6>
         </Button>
       </Form>
     </Container>
