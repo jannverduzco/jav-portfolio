cfc24eeff4d672330579f8b9bd0bf1e5c85f786f
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-17 01:55:44 -0500

    listed linkedIn, Resume and Github icons with links in footer

--- src/Componets/Footer/index.js
+++ src/Componets/Footer/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Resume from "../../images/JAVResume.pdf"
 import "./style.css";
 
 function Footer() {
   return (
     <footer>
       <Row className="justify-content-md-center">
-        <h4>Lets Connect</h4>
+        <h4>Let's Connect</h4>
       </Row>
       <Row className="justify-content-md-center">
         <div className="icons">
@@ -18,9 +18,14 @@ function Footer() {
           <a href="https://www.linkedin.com/in/jannethaverduzco">
             <i className="fab fa-linkedin" title="linked in" id="linkedIn"></i>
           </a>
+          <a href={Resume} download>
+          <i className="far fa-file" title="resume" id="resume"></i>
+          </a>
         </div>
       </Row>
-      <Row className="justify-content-md-center">All Rights Reserved</Row>
+      <Row className="justify-content-md-center">
+        @ 2021 Janenth Alvarado Verduzco
+      </Row>
     </footer>
   );
 }
