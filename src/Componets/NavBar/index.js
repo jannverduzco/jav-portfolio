aa2eab1529bc7ce4036167ef591d643072c4f02c
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-18 16:24:03 -0500

    Added favicon.ico file. Added Favico.ico to manifest.json

--- src/Componets/NavBar/index.js
+++ src/Componets/NavBar/index.js
@@ -3,6 +3,7 @@ import "./style.css";
 import * as ReactBootStrap from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+
 function NavBar() {
   return (
     <ReactBootStrap.Navbar
@@ -11,6 +12,7 @@ function NavBar() {
       expand="md"
       variant="light"
     >
+
       <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
       <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
         <ReactBootStrap.Nav className="mr-auto">
