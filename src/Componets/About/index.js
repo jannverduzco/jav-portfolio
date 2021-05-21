a41d4e84d24eb75b17bcb2b007794aca91d0b090
Janneth Verduzco <janneth_verduzco@live.com>
2021-05-21 00:21:13 -0500

    About me paragraph were cutting off in mobile responsiveness, simply added 'container-fluid' class

--- src/Componets/About/index.js
+++ src/Componets/About/index.js
@@ -4,19 +4,27 @@ import JannethV from "../../images/jannethaverduzco.jpg";
 
 function About() {
   return (
-    <div className="container">
+    <div className="container-fluid">
       <h2>About Me</h2>
       <div className="row about-me-section">
-        <div className="col-md-5">
-        <img src={JannethV} alt="Janneth Verduzco" height={320} width={420} />
+        <div className="col-md-4">
+          <img src={JannethV} alt="Janneth Verduzco" height={320} width={420} />
         </div>
         <div className="col-md-6">
-          I live in San Antonio, Texas, where I graduated from The University of
-          Texas with a Biology Degree. I used my degree to focus on clinical
-          trials. Four years post-graduation I have received my certificate,
-          also from UTSA, as a Full Stack Web Developer. I have experience as a
-          front and back end with a stack in JavaScript, React, MongoDB, MySQL,
-          NoSQL, GitHub.
+          <p>
+            I live in San Antonio, Texas, where I graduated from The University
+            of Texas with a Biology Degree. I used my degree to focus on
+            clinical trials.
+          </p>
+          <p>
+            {" "}
+            Four years post-graduation I have received my certificate, also from
+            UTSA, as a Full Stack Web Developer.
+          </p>
+          <p>
+            I have experience as a front and back end with a stack in
+            JavaScript, React, MongoDB, MySQL, NoSQL, GitHub.
+          </p>
         </div>
       </div>
     </div>
